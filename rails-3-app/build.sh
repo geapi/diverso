8983462f259816862fe1a404d74b12ae805c1248
Georg Apitz <georg@usebar.org>
2013-11-12 13:02:47 -0700

    update build.sh to include time stamp and summary

--- rails-3-app/build.sh
+++ rails-3-app/build.sh
@@ -1,49 +1,70 @@
 #!/bin/bash
+tput setaf 3
+echo "****************************************"
+echo "Running suite for Rails app ..."
+# install bundler if necessary
+#set -e
 
-# for selenium headless
-export DISPLAY=:99
+CURRENTDIR=`pwd`
+TEST_TO_RUN=$1
 
-source /usr/local/rvm/scripts/rvm
+exitStatus=0
 
-# must happen after loading rvm
-# set -e
+function run_app_specs {
+  echo "Tests for Rails App ..."
+  echo ""
+  source .rvmrc
+  echo USER=$USER && ruby --version && which ruby && which bundle
+  gem install bundler --no-ri --no-rdoc && bundle install --quiet
 
-export RUNNING_CI=true
+  if [[ -n "$JENKINS_HOME" ]]; then
+    ln -sf ../../shared/database.yml config/database.yml
+    RAILS_ENV=test bundle exec rake db:drop db:create db:migrate
+    CI=true rspec spec -t ~local_only
+    exitStatus+=$?
+    echo "\nBEGIN License Finder"
+    license_finder --quiet
+    echo "END License Finder\n"
+    exitStatus+=$?
+  else
+    RAILS_ENV=test bundle exec rake db:create db:migrate db:test:prepare
+    rspec spec
+    exitStatus+=$?
+  fi
+  exitStatus+=$?
+}
 
-rm spec/fixtures/*.yml
+function run_engine_tests {
+    echo "Tests for engines ..."
+    exitStatus+=$?
 
-rvm list | grep ruby-1.9.2-p180 > /dev/null || rvm install ruby-1.9.2-p180
+}
 
-rvm use ruby-1.9.2-p180@[your-rails-rvm] --create
+case "$1" in
 
-gem list --local bundler | grep bundler || gem install bundler
+lib)  echo "Running only tests for engines ..."
+      run_engine_tests
+      ;;
+*) echo "Running all tests ..."
+   run_app_specs
+    ;;
+esac
 
-bundle check || bundle install --local
+cd "$CURRENTDIR"
 
-echo "loading schema"
-RAILS_ENV=test bundle exec rake db:schema:load > /dev/null 2>&1
 
-declare -i ERROR_COUNT=0
 
-echo "running specs"
-RAILS_ENV=test bundle exec rspec spec -t ~js
-ERROR_COUNT+=$?
-echo "ERROR_COUNT $ERROR_COUNT"
-
-echo "running selenium"
-RAILS_ENV=test bundle exec rspec spec -t js
-ERROR_COUNT+=$?
-echo "ERROR_COUNT $ERROR_COUNT"
-
-echo "running jasmine tests"
-RAILS_ENV=test bundle exec rake jasmine:ci:headless
-ERROR_COUNT+=$?
-echo "ERROR_COUNT $ERROR_COUNT"
-
-# make sure the test fixtures still load
-echo "loading test fixtures"
-RAILS_ENV=development bundle exec rake db:rebuild
-ERROR_COUNT+=$?
-echo "ERROR_COUNT $ERROR_COUNT"
-
-exit $ERROR_COUNT
+if [ $exitStatus -eq 0 ]; then
+tput setaf 2
+echo "****************************************"
+echo "**         SUCCESSFUL BUILD!          **"
+echo "**    `date`    **"
+echo "****************************************"
+else
+tput setaf 1
+echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+echo "XX           BUILD FAILED!            XX"
+echo "XX    `date`    XX"
+echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+exit -1
+fi
