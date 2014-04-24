1b5a095d48d69b9ca88b97f65428055aa3a20670
Georg Apitz <georg@usebar.org>
2014-04-24 10:58:42 -0600

    add git aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,1 +1,4 @@
 alias rmo="find . -name *.orig -exec rm -f {} \;"
+alias gl='git log --pretty=format:'\''%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s'\'' --date=short'
+alias gpr='git pull --rebase'
+alias gs='git status'
